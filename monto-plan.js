fc12c7bfb77fa2adcf1d388ba6d23f0df7d57cc1
Cdelgado1211 <cdelgado@danaconnect.com>
2026-04-30 11:44:49 -0400

    Add files via upload

--- monto-plan.js
+++ monto-plan.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   var montoInput = document.querySelector(".montoPlan input");
-  var opcion = document.querySelector("#li_4 label");
+  var opcion = document.querySelector("#li_2 label");
 
   if (montoInput && opcion) {
     opcion.innerHTML = opcion.innerHTML.replace("XXX", montoInput.value);
